8f8d30a4d915aa5bc9cd9279013feeda3cdadef0
Marcin Bychawski <marcin@bychawski.net>
2017-10-22 02:42:11 +0200

    A huge commit; all done

--- README.md
+++ README.md
@@ -4,4 +4,12 @@
 ## TODO steps
 1. Użyj `create-react-app` do stworzenia projektu
 1. Wywołaj skrypt `npm run eject`
-1. zainstaluj `
+1. zainstaluj `babel-plugin-transform-decorators-legacy`
+1. Dodaj do kofiguracji babela:
+  ```
+  "plugins": [
+    "transform-decorators-legacy"
+  ]
+  ```
+
+1. Create rootStore, apiConnector,
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {useStrict as mobxUseStrict} from 'mobx';
 import registerServiceWorker from './registerServiceWorker';
+import 'semantic-ui-css/semantic.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import RootStore from 'stores';
+import App from 'components/App';
+
+mobxUseStrict();
+
+const fetch = window.fetch;
+const rootStore = new RootStore(fetch);
+
+ReactDOM.render(
+  <App rootStore={rootStore} />,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
+
+window.rootStore = rootStore;
